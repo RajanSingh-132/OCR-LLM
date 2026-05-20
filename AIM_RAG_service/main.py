32596816a2a31ab14204d456e7ac1b86da595b8f
RajanSingh-132 <singhrajan7052@gmail.com>
2026-05-20 18:29:38 +0530

    chages in file strucutrue and toml

--- AIM_RAG_service/main.py
+++ AIM_RAG_service/main.py
@@ -1,14 +1,15 @@
 import uvicorn
 import os
 
+from app.fast_api_actions_session_rag import app
+
 HOST = os.getenv("HOST", "127.0.0.1")
 PORT = int(os.getenv("PORT", 8090))
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.fast_api_actions_session_rag:app",
+        "main:app",
         host=HOST,
         port=PORT,
         reload=True,
     )
-    
